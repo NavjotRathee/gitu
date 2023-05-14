f1050d621cfe09d1366491fc619b786b01640c5d
Navjot Rathee <navjotrathee@gmail.com>
2023-05-14 23:28:03 +0530

    3

--- orangepear/src/App.js
+++ orangepear/src/App.js
@@ -6,8 +6,10 @@ function App() {
   
 
   return (
+    <>
+    
    <Nav />
-      
+   </>
   );
 }
 
--- orangepear/src/components/Nav.js
+++ orangepear/src/components/Nav.js
@@ -2,46 +2,56 @@ import React from "react";
 import './Nav.css';
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagramSquare } from "react-icons/fa";
+
+
 const Nav=()=>{
+    
     return(
         <>
         <nav className="main-nav">
             <div className="logo">
-            <img src="./Images/logo.png" alt="logo"/>
+            <img src="./Images/logo.png" alt="logo" />
             <h2><span>O</span>range <span>P</span>ear</h2>
             </div>
             <div className="menu-link">
                 <ul>
                     <li>
-                        <a href="#">Home</a>
+                        <a href="home">Home</a>
                     </li>
                     <li>
-                        <a href="#">About Us</a>
+                        <a href="about-us">About Us</a>
                     </li>
                     <li>
-                        <a href="#">Shop Now</a>
+                        <a href="shop">Shop Now</a>
                     </li>
                     <li>
-                        <a href="#">Contact Us</a>
+                        <a href="contact-us">Contact Us</a>
                     </li>
                 </ul>
             </div>
             <div className="socials">
                 <ul>
                     <li>
-                        <a href="https://www.facebook.com/orangepear.co.in/" target="_OP"><FaFacebook /></a>
+                        <a href="https://www.facebook.com/orangepear.co.in/" target="_OP"><FaFacebook className="fb" /></a>
                     </li>
                     <li>
-                        <a href="https://www.facebook.com/orangepear.co.in/" target="_OP1"><FaInstagramSquare /></a>
+                        <a href="https://www.facebook.com/orangepear.co.in/" target="_OP1"><FaInstagramSquare className="instagram" /></a>
                     </li>
                 </ul>
             </div>
         </nav>
-        <section className="hero-page">
-            
+        <section className="hero-section">
+            <h2>Welcome to OP</h2>
+        </section>
+        
+        <section className="about-us">
+        <div>
+            <p>Your fashion,our passion</p>
+        </div>
         </section>
           
         </>
     )
+    
 }
 export default Nav;
